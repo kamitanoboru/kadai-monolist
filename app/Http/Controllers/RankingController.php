225d1587d903d9f00e03a6b5a1ea22fed1afb3ae
Noboru.kamita <noboru.kamita@gamil.com>
2018-10-12 12:57:07 +0000

    ranking

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -4,26 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Controllers\Controller;
 
 use App\Item;
 
-class WelcomeController extends Controller
+class RankingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $items = Item::orderBy('updated_at', 'desc')->paginate(20);
-
-        return view('welcome', [
-            'items' => $items,
-        ]);
-    }
-    
-    
     public function want()
     {
         $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'want')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('count', 'DESC')->take(10)->get();
--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,6 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['show']]);
 });
 
+// ランキング
+Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
+
